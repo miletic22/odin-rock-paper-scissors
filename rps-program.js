a836c5d45ae9001d5502f1e3a535e11eab52427d
Goran <goran.miletic122@gmail.com>
2022-07-20 20:58:38 +0200

    Create a function to get computer's choice
    - function has no arguments
    - creates a random number between 1-3
    - assigns a string of "Rock/ Paper/ Scissors"
      based on the number (1/ 2/ 3).
    - HTML document is changed to have a black bckgd.

---
+++ rps-program.js
@@ -0,0 +1,27 @@
+let compChoice;
+let playerChoice;
+
+function getComputerChoice() {
+    compChoice = Math.floor(Math.random() * 3 + 1);
+    
+    switch (compChoice) {
+        case 1: 
+            compChoice = "Rock";
+            break;
+
+        case 2: 
+            compChoice = "Paper";
+            break;
+
+        case 3: 
+            compChoice = "Scissors";
+            break;
+        default:
+            console.log("Number is not 1-3, something went wrong.")
+}
+    return compChoice;
+}
+
+
+
+getComputerChoice();
