f33053d46cf885626e47ca6d5f4aae6d5de8ca8f
Goran <goran.miletic122@gmail.com>
2022-07-21 16:22:14 +0200

    Fix logical errors, update the code
    Fixed another logical error and fixed the code
    Updated to fit the TOP criteria.

--- rps-program.js
+++ rps-program.js
@@ -1,7 +1,7 @@
-let computerSelection;
-let playerSelection;
-
+let computerScore = 0;
+let playerScore = 0;
 function getComputerChoice() {
+    let computerSelection;
     switch (Math.floor(Math.random() * 3 + 1)) { // Creates a random number between 1-3
         case 1: 
             computerSelection = "rock";
@@ -22,49 +22,72 @@ function getComputerChoice() {
 }
 
 function getPlayerChoice() {
+    let playerSelection;
     playerSelection = window.prompt("What's your choice?\n Rock\n Paper \n Scissors");
     playerSelection = playerSelection.toLowerCase();
     return playerSelection;
-
 }
 
 
-computerSelection = getComputerChoice();
-playerSelection = getPlayerChoice();
 
-function playGame (computerSelection, playerSelection) {
-
+function playRound (computerSelection, playerSelection) {
 
     if (playerSelection == computerSelection) {
-        return console.log("It's a tie!");
+        return console.log(`It's a tie! (${playerSelection} == ${computerSelection}) `);
     }
     
     if (playerSelection == "rock") {
         if (computerSelection == "scissors") {
+            playerScore++;
             return console.log("You win! Rock beats Scissors!");
         }
+
         else {
+            computerScore++;
             return console.log("You lose! Rock loses to Paper!");
         }
     }
 
     if (playerSelection == "paper") {
-        if (computerSelection == "scissors") {
-            return console.log("You lose! Paper loses to Scissors!");
-        }
-        else {
+        if (computerSelection == "rock") {
+            playerScore++;
             return console.log("You win! Paper beats Rock!");
+        }
+
+        else {
+            computerScore++;
+            return console.log("You lose! Paper loses to Scissors!");
         }
     }
 
     if (playerSelection == "scissors") {
-        if (computerSelection == "rock") {
-            return console.log("You lose! Scissors loses to Rock!");
-        }
-        else {
+        if (computerSelection == "paper") {
+            playerScore++;
             return console.log("You win! Scissosrs beats Paper!");
+        }
+
+        else {
+            computerScore++;
+            return console.log("You lose! Scissors loses to Rock!");
         }
     }
 }
 
- playGame (computerSelection, playerSelection);
+function game() {
+    for (let i = 0; i < 5; i++) {
+        computerSelection = getComputerChoice();
+        playerSelection = getPlayerChoice();
+        playRound(computerSelection, playerSelection);
+        console.log(`Computer chose: ${computerSelection} || Player chose: ${playerSelection}`);
+        console.log(`Computer score: ${computerScore}) || Player score: ${playerScore}`);
+        console.log("_")
+     }
+     if (computerScore > playerScore) {
+        console.log("You lost.")
+     }
+     else {
+        console.log("You won!");
+     }
+}
+ 
+game();
