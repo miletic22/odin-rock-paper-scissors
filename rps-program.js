e59809da98cafe66485fb6bda63f09be008ce529
Goran <goran.miletic122@gmail.com>
2022-07-21 14:58:43 +0200

    Fix logical error & complete the committed
    There was a logical error with the way functions
    were interracting with the global variables, fixed
    that and added the rest of the code. The game works
    as intended

--- rps-program.js
+++ rps-program.js
@@ -1,32 +1,66 @@
-let compChoice;
-let playerChoice;
-
 function getComputerChoice() {
-    compChoice = Math.floor(Math.random() * 3 + 1);
-    
-    switch (compChoice) {
+    switch (Math.floor(Math.random() * 3 + 1)) { // Creates a random number between 1-3
         case 1: 
-            compChoice = "Rock";
+            compChoice = "rock";
             break;
 
         case 2: 
-            compChoice = "Paper";
+            compChoice = "paper";
             break;
 
         case 3: 
-            compChoice = "Scissors";
+            compChoice = "scissors";
             break;
         default:
             console.log("Number is not 1-3, something went wrong.")
 }
     return compChoice;
+
 }
 
 function getPlayerChoice() {
     playerChoice = window.prompt("What's your choice?\n Rock\n Paper \n Scissors");
+    playerChoice = playerChoice.toLowerCase();
     return playerChoice;
+
 }
 
 
 
-getComputerChoice();
+function playGame (g) {
+    let compChoice = getComputerChoice();
+    let playerChoice = getPlayerChoice();
+
+    if (playerChoice == compChoice) {
+        return console.log("It's a tie!");
+    }
+    
+    if (playerChoice == "rock") {
+        if (compChoice == "scissors") {
+            return console.log("You win! Rock beats Scissors!");
+        }
+        else {
+            return console.log("You lose! Rock loses to Paper!");
+        }
+    }
+
+    if (playerChoice == "paper") {
+        if (compChoice == "scissors") {
+            return console.log("You lose! Paper loses to Scissors!");
+        }
+        else {
+            return console.log("You win! Paper beats Rock!");
+        }
+    }
+
+    if (playerChoice == "scissors") {
+        if (compChoice == "rock") {
+            return console.log("You lose! Scissors loses to Rock!");
+        }
+        else {
+            return console.log("You win! Scissosrs beats Paper!");
+        }
+    }
+}
+
+playGame();
